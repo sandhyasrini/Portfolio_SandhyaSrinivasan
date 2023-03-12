fa13c0f97e7b030b9e838bca4f8eb0f8f9d7a252
Sandhya Srinivasan <sandy150225@gmail.com>
2023-03-12 18:26:52 +0100

    changed expertise text

--- utils/config.js
+++ utils/config.js
@@ -39,7 +39,7 @@ const about_me = [
   link_text: 'Certified Kubernetes Application developer',
   link_url: 'https://www.credly.com/badges/818c1f5f-17ad-45dd-8a78-73fddc273b3f',
   target: '_blank',
-  connector: ` and a `,
+  connector: ` and an `,
   second_text: ' AWS Certified Developer Associate',
   second_url: 'https://www.credly.com/badges/2ede14c3-7d8b-4172-9cfb-377e53a2d249/public_url'
   },
