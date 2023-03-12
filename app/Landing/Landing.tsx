d6499d106df43900958b7a677c43c6c570fb97d3
Sandhya Srinivasan <sandy150225@gmail.com>
2023-03-12 15:16:47 +0100

    minor button positioning css changes

--- app/Landing/Landing.tsx
+++ app/Landing/Landing.tsx
@@ -18,12 +18,12 @@ export default function Landing() {
         <h1>a Full Stack developer</h1>
       </div>
       <p
-        className={`font-thin text-lg text-white my-[4rem] mx-0 description`}
+        className={`font-thin text-lg text-white my-[4rem] mx-0 description text-justify`}
       >
         {description}
       </p>
       <a href="/#contact"
-        className="text-[#009d68] w-[40%] text-[0.5rem] sm:w-[40%] sm:text-[1rem] sm:text-lg h-[100%] md:w-[20%] transition ease-in-out delay-150 bg-gray border border-[#009d68] focus:outline-none hover:bg-[#009d68] hover:text-white focus:ring-4 hover:-translate-y-1 hover:scale-110 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 align-middle text-center"
+        className="text-[#009d68] w-[50%] text-[0.5rem] sm:w-[40%] sm:text-[1rem] sm:text-lg h-[100%] md:w-[20%] transition ease-in-out delay-150 bg-gray border border-[#009d68] focus:outline-none hover:bg-[#009d68] hover:text-white focus:ring-4 hover:-translate-y-1 hover:scale-110 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 align-middle text-center"
       >
         Contact me!
       </a>{" "}
