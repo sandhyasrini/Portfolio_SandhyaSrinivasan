42e3263ed4e21990a0274918ebab5c395b578184
Sandhya Srinivasan <sandy150225@gmail.com>
2023-03-25 13:40:13 +0100

    fixed intersectionobserver api error during build

--- app/components/UseIsInViewPort.tsx
+++ app/components/UseIsInViewPort.tsx
@@ -1,24 +1,29 @@
-import { RefObject, useEffect, useMemo, useState } from "react";
+import { RefObject, useEffect, useMemo, useRef, useState } from "react";
 
 export function useIsInViewport(ref: RefObject<HTMLElement>) {
-    const [isIntersecting, setIsIntersecting] = useState(false);
-  
-    const observer = useMemo(
-      () =>
-        new IntersectionObserver(([entry]) =>
-          setIsIntersecting(entry.isIntersecting),
-        ),
-      [],
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const observer: any = useRef();
+
+  // observer = useMemo(
+  //   () =>
+  //     new IntersectionObserver(([entry]) =>
+  //       setIsIntersecting(entry.isIntersecting)
+  //     ),
+  //   []
+  // );
+  useEffect(() => {
+    observer.current = new IntersectionObserver(([entry]) =>
+      setIsIntersecting(entry.isIntersecting)
     );
-  
-    useEffect(() => {
-      observer.observe(ref.current as Element);
-  
-      return () => {
-        observer.disconnect();
-      };
-    }, [ref, observer]);
-  
-    return isIntersecting;
-  }
-  
+  }, []); // do this only once, on mountnpm 
+
+  useEffect(() => {
+    observer && observer.current.observe(ref.current as Element);
+
+    return () => {
+      observer && observer.current.disconnect();
+    };
+  }, [ref, observer]);
+
+  return isIntersecting;
+}
